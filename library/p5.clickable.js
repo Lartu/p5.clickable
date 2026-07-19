16ba36bd98799b7ac2c2466fa4c7abbad28c5b0b
Lartu <lartu@lartu.net>
2026-07-19 03:57:23 -0300

    Update p5.clickable.js

--- library/p5.clickable.js
+++ library/p5.clickable.js
@@ -1,50 +1,50 @@
-var _p5_clickable_clickables = [];
-
-function Clickable() {
-	this.x = 0;
-	this.y = 0;
-	this.width = 100;
-	this.height = 100;
-	this.__zorder = 0;
-
-	this.locate = function (x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	this.resize = function (w, h) {
-		this.width = w;
-		this.height = h;
-	}
-
-	this.setZ = function (zorder) {
-		this.__zorder = zorder;
-	}
-
-	this.onPress = function () {
-	}
-
-	this.isPressed = function () {
-	}
-
-	this.onRelease = function () {
-	}
-
-	this.onHoverStart = function () {
-	}
-
-	this.isHovered = function () {
-	}
-
-	this.onHoverEnd = function () {
-	}
-
-	_p5_clickable_clickables.push(this);
-}
-
 function p5ClickableAddon(p5, fn, lifecycles) {
 	fn.createClickable = function () {
-		return new Clickable();
+		return new __p5ClickableObject();
+	}
+
+	fn.__p5_clickable_clickables = [];
+
+	fn.__p5ClickableObject = function () {
+		this.x = 0;
+		this.y = 0;
+		this.width = 100;
+		this.height = 100;
+		this.__zorder = 0;
+
+		this.locate = function (x, y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		this.resize = function (w, h) {
+			this.width = w;
+			this.height = h;
+		}
+
+		this.setZ = function (zorder) {
+			this.__zorder = zorder;
+		}
+
+		this.onPress = function () {
+		}
+
+		this.isPressed = function () {
+		}
+
+		this.onRelease = function () {
+		}
+
+		this.onHoverStart = function () {
+		}
+
+		this.isHovered = function () {
+		}
+
+		this.onHoverEnd = function () {
+		}
+
+		fn.__p5_clickable_clickables.push(this);
 	}
 
 	lifecycles.predraw = function () {
